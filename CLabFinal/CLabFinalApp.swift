17b580acbed492449d5a9810724cbf4225505eb5
Taekyun Kim <taekkim@student.42seoul.kr>
2023-06-18 14:48:08 +0900

    Initial Commit

---
+++ CLabFinal/CLabFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLabFinalApp.swift
+//  CLabFinal
+//
+//  Created by taekkim on 2023/06/18.
+//
+
+import SwiftUI
+
+@main
+struct CLabFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
